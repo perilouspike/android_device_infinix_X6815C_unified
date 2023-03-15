a079843d7164ca3efae44dfe1014ce108516ff9a
Shibu Shaji <97983211+perilouspike@users.noreply.github.com>
2023-03-15 13:04:09 +0530

    Removed custom Flashlight path.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -141,9 +141,9 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
     # SAR detection.
 	export OF_USE_TWRP_SAR_DETECT=1
     # Enable the flashlight feature.
-	export OF_FLASHLIGHT_ENABLE=1
+	#export OF_FLASHLIGHT_ENABLE=1
 	# Set custom flashlight path.
-	export OF_FL_PATH1="/sys/devices/virtual/torch/torch/torch_level"
+	#export OF_FL_PATH1="/sys/devices/virtual/torch/torch/torch_level"
 
 	# let's see what are our build VARs
 	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
